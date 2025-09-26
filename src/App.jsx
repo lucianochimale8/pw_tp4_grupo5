417dbc746a6008eeb8dc5c169083a02a99d8b5ec
fátima <fatimacatacata51@gmail.com>
2025-09-26 17:39:38 -0300

    Saludo en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
 ﻿
+import React from "react";
+import Saludo from "./assets/components/Saludo";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <Saludo nombre="Luciano" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Saludo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Saludo({ nombre }) {
+  return (
+    <div className="p-4 bg-blue-200 rounded-xl shadow-md text-center">
+      <h1 className="text-2xl font-bold">¡Hola, {nombre}!</h1>
+      <p className="text-gray-700">Nuestro primer saludo en react </p>
+    </div>
+  );
+}
+
+export default Saludo;
